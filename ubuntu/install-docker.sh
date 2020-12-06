d5c3362cf13eb82e994d45a6574d6da2d9d00d59
Raúl Correia <raulcorreia7@gmail.com>
2020-12-06 15:32:02 +0000

    Added ubuntu script to install docker on 20.10

---
+++ ubuntu/install-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu groovy test"
+sudo apt instal docker-ce docker-ce docker-ce-cli containerd.io -y
